3adc00839810341d1a811a5ff7a1d1f13da28ade
jdsteadycode <desaijeet11@gmail.com>
2025-08-09 13:13:06 +0000

    added separate Profile.js Component

--- src/Profile.js
+++ src/Profile.js
@@ -1,4 +1,6 @@
 // () -> Profile Component
 export default function Profile() {
-  return <img src="https://i.imgur.com/QIrZWGIs.jpg" alt="Alan L. Hart" />;
+  return (
+    <img src="https://i.imgur.com/QIrZWGIs.jpg" alt="image for Alan L. Hart" />
+  );
 }
